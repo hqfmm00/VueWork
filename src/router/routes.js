c32e2f85702a9a2e566056489cfa3c88fc860c0d
MIniCat <827351411@qq.com>
2019-12-18 14:46:52 +0800

    shop布局

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,12 @@ import Search from '@/pages/Search/Search'
 import Order from '@/pages/Order/Order'
 import Profile from '@/pages/Profile/Profile'
 import Login from '@/pages/Login/Login'
+import Shop from '@/pages/Shop/Shop'
+import Goods from '@/pages/Shop/Goods'
+import Info from '@/pages/Shop/Info'
+import Ratings from '@/pages/Shop/Ratings'
+
+
 
 
 export default [
@@ -37,6 +43,28 @@ export default [
   {
     path:'/login',
     component:Login
+  },
+  {
+    path:'/shop',
+    component:Shop,
+    children:[
+      {
+        path:'/shop/goods',
+        component:Goods
+      },
+      {
+        path:'/shop/ratings',
+        component:Ratings
+      },
+      {
+        path:'/shop/info',
+        component:Info
+      },
+      {
+        path:'/',
+        redirect:'/shop/goods'
+      },
+    ]
   },
   {
     path:'/',
